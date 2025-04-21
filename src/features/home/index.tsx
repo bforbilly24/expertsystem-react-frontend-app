89f01bd502b96d63214aa9871d05a05a112a6989
Halim <halimputra0701@gmail.com>
2025-04-21 15:55:51 +0700

    feat(page): adding home page

--- src/features/home/index.tsx
+++ src/features/home/index.tsx
@@ -1,56 +1,16 @@
-import { Header } from '@/components/layout/header'
+import AnimationContainer from '@/components/global/animation-container'
+import MaxWidthWrapper from '@/components/global/max-width-wrapper'
 import { Main } from '@/components/layout/main'
-import { TopNav } from '@/components/layout/top-nav'
-import { ThemeSwitch } from '@/components/theme-switch'
-import { Button } from '@/components/ui/button'
+import Questions from './_components/questions'
 
 export default function Home() {
   return (
-    <>
-      {/* ===== Top Heading ===== */}
-      <Header>
-        <TopNav links={topNav} />
-        <div className='ml-auto flex items-center space-x-4'>
-          <ThemeSwitch />
-        </div>
-      </Header>
-
-      {/* ===== Main ===== */}
-      <Main>
-        <div className='mb-2 flex items-center justify-between space-y-2'>
-          <h1 className='text-2xl font-bold tracking-tight'>Test</h1>
-          <div className='flex items-center space-x-2'>
-            <Button>Download</Button>
-          </div>
-        </div>
-      </Main>
-    </>
+    <AnimationContainer>
+      <MaxWidthWrapper>
+        <Main className='pt-32 pb-24'>
+          <Questions />
+        </Main>
+      </MaxWidthWrapper>
+    </AnimationContainer>
   )
 }
-
-const topNav = [
-  {
-    title: 'Home',
-    href: '/',
-    isActive: true,
-    disabled: false,
-  },
-  {
-    title: 'Customers',
-    href: 'dashboard/customers',
-    isActive: false,
-    disabled: true,
-  },
-  {
-    title: 'Products',
-    href: 'dashboard/products',
-    isActive: false,
-    disabled: true,
-  },
-  {
-    title: 'Settings',
-    href: 'dashboard/settings',
-    isActive: false,
-    disabled: true,
-  },
-]
