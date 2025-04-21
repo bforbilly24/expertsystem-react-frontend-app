fafb091f628eadf618912aacdf6ff37abbfc20c9
Halim <halimputra0701@gmail.com>
2025-04-21 16:09:33 +0700

    feat(route): adding __root

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -2,16 +2,17 @@ import { QueryClient } from '@tanstack/react-query'
 import { createRootRouteWithContext, Outlet } from '@tanstack/react-router'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import { TanStackRouterDevtools } from '@tanstack/router-devtools'
-import { Toaster } from '@/components/ui/toaster'
+import { Toaster } from '@/components/ui/shadcn/toaster'
 import GeneralError from '@/features/errors/general-error'
 import NotFoundError from '@/features/errors/not-found-error'
+import { MainLayout } from '@/layouts/main-layout'
 
 export const Route = createRootRouteWithContext<{
   queryClient: QueryClient
 }>()({
   component: () => {
     return (
-      <>
+      <MainLayout>
         <Outlet />
         <Toaster />
         {import.meta.env.MODE === 'development' && (
@@ -20,7 +21,7 @@ export const Route = createRootRouteWithContext<{
             <TanStackRouterDevtools position='bottom-right' />
           </>
         )}
-      </>
+      </MainLayout>
     )
   },
   notFoundComponent: NotFoundError,
