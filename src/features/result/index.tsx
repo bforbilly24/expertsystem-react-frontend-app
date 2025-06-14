c47e5baa21a139aaef9ecace8d192bc727136296
Halim <halimputra0701@gmail.com>
2025-06-14 14:18:03 +0700

    efactor(page): update content as need, adding result data fallback

--- src/features/result/index.tsx
+++ src/features/result/index.tsx
@@ -1,4 +1,7 @@
-import { useLocation, Link } from '@tanstack/react-router'
+import { useEffect } from 'react'
+import { Link, useLocation, useNavigate } from '@tanstack/react-router'
+import { SlashIcon } from 'lucide-react'
+import { ROLE_RESULTS } from '@/constants/role-results'
 import {
   Breadcrumb,
   BreadcrumbItem,
@@ -9,12 +12,32 @@ import {
 import AnimationContainer from '@/components/global/animation-container'
 import MaxWidthWrapper from '@/components/global/max-width-wrapper'
 import { Main } from '@/components/layout/main'
+import { RecommendationCard } from './_components/recommendation-card'
 import ResultInfo from './_components/result_info'
-import { SlashIcon } from 'lucide-react'
 
 export default function Result() {
   const { state } = useLocation()
-  const result = state?.result ?? 'No result available'
+  const navigate = useNavigate()
+
+  const isValidRole = (key: string): key is keyof typeof ROLE_RESULTS => {
+    return key in ROLE_RESULTS
+  }
+
+  const result = state?.result
+  const hasValidResult = result && isValidRole(result)
+
+  useEffect(() => {
+    if (!hasValidResult) {
+      navigate({ to: '/' })
+      return
+    }
+  }, [hasValidResult, navigate])
+
+  if (!hasValidResult || !result) {
+    return null
+  }
+
+  const resultData = ROLE_RESULTS[result as keyof typeof ROLE_RESULTS]
 
   return (
     <AnimationContainer>
@@ -46,8 +69,18 @@ export default function Result() {
               menemukan peran yang paling cocok untukmu.
             </p>
           </div>
+          <ResultInfo role={resultData} />
 
-          <ResultInfo role={result} />
+          <div className='flex flex-col gap-y-2 mt-8 items-start justify-center'>
+            <h1 className='text-2xl capitalize font-semibold text-orange-500'>
+              Langkah Selanjutnya untuk Mengasah Bakatmu
+            </h1>
+            <h6 className='text-base text-muted-foreground'>
+              Kamu selangkah lebih dekat ke karier impianmu! Berikut adalah
+              pelatihan untuk membantu kamu mengasah bakat dan keterampilanmu.
+            </h6>
+          </div>
+          <RecommendationCard courses={resultData.courses} />
         </Main>
       </MaxWidthWrapper>
     </AnimationContainer>
